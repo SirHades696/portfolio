e0a76a4ee8237a6ad71dcd2b6bcf3204faa112df
SirHades696 <47828202+SirHades696@users.noreply.github.com>
2022-04-11 19:15:22 -0500

    updating projects in portfolio

--- app/projects.py
+++ app/projects.py
@@ -4,16 +4,17 @@ __email__ = 'djnonasrm@gmail.com'
 
 projects = {
     1 : ['Portfolio👽', 'Aplicación web hecha con Flask para mostrar el portafolio de proyectos desplegados en GitHub y un formulario de contacto.', 'https://github.com/SirHades696/portfolio'],
-    2 : ['Anime Bot Telegram🤖','Bot disponible en telegram que se encarga de buscar animes y sus respectivos episodios a través de web scraping. ', 'https://github.com/SirHades696/animebot-python'],
-    3 : ['To Do App📝','Aplicación web encarga de crear una lista de tareas para diferentes usuarios, mostradas a través de stick notes, esta app fue desarrollada con Flask.', 'https://github.com/SirHades696/lista_tareas_flask'],
-    4 : ['PyPi Bot Telegram🤖','Bot disponible en Telegram que se encarga de buscar información acerca de paquetes para Python en la plataforma de PyPi.org.', 'https://github.com/SirHades696/Pypi_bot_telegram'],
-    5 : ['Mailer App📧','Aplicación web desarrollada con Flask para enviar correos electrónicos con SendGrid y almacenarlos en una Base de Datos.', 'https://github.com/SirHades696/flask_mailer'],
-    6 : ['Virtual Assistant🤖','Asistente virtual desarrollado con Python, el cual realiza tareas sencillas de búsqueda en Wikipedia, reproduce música en YouTube, etc. ', 'https://github.com/SirHades696/VirtualAssist'],
-    7 : ['Warzone Stats App🎮','Aplicación desarrollada en Python la cual muestra las estadísticas de jugadores de Call of Duty: Warzone de diferentes plataformas.', 'https://github.com/SirHades696/Warzone_python'],
-    8 : ['Basic Calculator📟','Calculadora básica desarrolla con Python la cual permite sumar, restar multiplicar y dividir.', 'https://github.com/SirHades696/Calculator'],
-    9 : ['Org. de Archivos📁', 'Clasificador de archivos de acuerdo con su extensión agrupándolos en carpetas, por default clasifica los archivos ubicados en “Descargas”.', 'https://github.com/SirHades696/ClasificadorArchivos'], 
-    10 : ['DataScience Spotify🎶', 'Script que analiza las canciones contenidas en una playlist para posteriormente realizar un gráfico basado en el tipo.', 'https://github.com/SirHades696/Spotify_and_Python'], 
-    11 : ['D.S. Pokemons🐞', 'Clasificación de pokemons basados en sus características de ataque, poder, entre otros; esta clasificación fue realizada con Pandas.', 'https://github.com/SirHades696/DataScience_Pokemons/'],
-    12 : ['D.S. Mundiales⚽','DataScience aplicado a un histórico de mundiales extraídos desde un archivo de hoja de cálculo, obteniendo información  como: goles promedio, goleadores, etc.','https://github.com/SirHades696/DataScience_'],
+    2: ['PokémonDB API🐛','API creada con Flask para mostrar las estadísticas de cada uno de los Pokémon, la información fue extraída de pokemondb.net', 'https://github.com/SirHades696/PokemonBD-API'],
+    3 : ['Anime Bot Telegram🤖','Bot disponible en telegram que se encarga de buscar animes y sus respectivos episodios a través de web scraping. ', 'https://github.com/SirHades696/animebot-python'],
+    4 : ['To Do App📝','Aplicación web encarga de crear una lista de tareas para diferentes usuarios, mostradas a través de stick notes, esta app fue desarrollada con Flask.', 'https://github.com/SirHades696/lista_tareas_flask'],
+    5 : ['PyPi Bot Telegram🤖','Bot disponible en Telegram que se encarga de buscar información acerca de paquetes para Python en la plataforma de PyPi.org.', 'https://github.com/SirHades696/Pypi_bot_telegram'],
+    6 : ['Mailer App📧','Aplicación web desarrollada con Flask para enviar correos electrónicos con SendGrid y almacenarlos en una Base de Datos.', 'https://github.com/SirHades696/flask_mailer'],
+    7 : ['Virtual Assistant🤖','Asistente virtual desarrollado con Python, el cual realiza tareas sencillas de búsqueda en Wikipedia, reproduce música en YouTube, etc. ', 'https://github.com/SirHades696/VirtualAssist'],
+    8 : ['Warzone Stats App🎮','Aplicación desarrollada en Python la cual muestra las estadísticas de jugadores de Call of Duty: Warzone de diferentes plataformas.', 'https://github.com/SirHades696/Warzone_python'],
+    9 : ['Basic Calculator📟','Calculadora básica desarrolla con Python la cual permite sumar, restar multiplicar y dividir.', 'https://github.com/SirHades696/Calculator'],
+    10 : ['Org. de Archivos📁', 'Clasificador de archivos de acuerdo con su extensión agrupándolos en carpetas, por default clasifica los archivos ubicados en “Descargas”.', 'https://github.com/SirHades696/ClasificadorArchivos'], 
+    11 : ['DataScience Spotify🎶', 'Script que analiza las canciones contenidas en una playlist para posteriormente realizar un gráfico basado en el tipo.', 'https://github.com/SirHades696/Spotify_and_Python'], 
+    12 : ['D.S. Pokemons🐞', 'Clasificación de pokemons basados en sus características de ataque, poder, entre otros; esta clasificación fue realizada con Pandas.', 'https://github.com/SirHades696/DataScience_Pokemons/'],
+    13 : ['D.S. Mundiales⚽','DataScience aplicado a un histórico de mundiales extraídos desde un archivo de hoja de cálculo, obteniendo información  como: goles promedio, goleadores, etc.','https://github.com/SirHades696/DataScience_'],
     
 }
