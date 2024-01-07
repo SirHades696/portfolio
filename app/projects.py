9a46cfc8c0710e1016a6d74e71efb70862d595fb
Emmanuel Nicanor <djnonasrm@gmail.com>
2024-01-06 18:42:31 -0600

    added Geoweb project

--- app/projects.py
+++ app/projects.py
@@ -4,76 +4,81 @@ __email__ = "djnonasrm@gmail.com"
 
 projects = {
     1: [
+        "Geoweb🌐",
+        "Portal Geoweb que consume servicios de Geoserver, PostgreSQL (PostGIS) y Leaflet.",
+        "https://github.com/SirHades696/GeoWeb",
+    ],
+    2: [
         "Portfolio👽",
         "Aplicación web hecha con Flask para mostrar el portafolio de proyectos desplegados en GitHub y un formulario de contacto.",
         "https://github.com/SirHades696/portfolio",
     ],
-    2: [
+    3: [
         "PokémonDB API🐛",
         "API creada con Flask para mostrar las estadísticas de cada uno de los Pokémon, la información fue extraída de pokemondb.net",
         "https://github.com/SirHades696/PokemonBD-API",
     ],
-    3: [
+    4: [
         "IP Sub and VLSM Calc🌐",
         "Aplicación web encargada de calcular subneto tradicional y VLSM desarollada con flask",
         "https://github.com/SirHades696/subnetting-calc-app",
     ],
-    4: [
+    5: [
         "Rick and Morty👽",
         "Aplicación web que utiliza la API de Rick and Morty, despliega los datos importantes de cada uno de los persoanjes de la serie.",
         "https://github.com/SirHades696/Rick_Morty/",
     ],
-    5: [
+    6: [
         "Anime Bot Telegram🤖",
         "Bot disponible en telegram que se encarga de buscar animes y sus respectivos episodios a través de web scraping. ",
         "https://github.com/SirHades696/animebot-python",
     ],
-    6: [
+    7: [
         "To Do App📝",
         "Aplicación web encarga de crear una lista de tareas para diferentes usuarios, mostradas a través de stick notes, esta app fue desarrollada con Flask.",
         "https://github.com/SirHades696/lista_tareas_flask",
     ],
-    7: [
+    8: [
         "PyPi Bot Telegram🤖",
         "Bot disponible en Telegram que se encarga de buscar información acerca de paquetes para Python en la plataforma de PyPi.org.",
         "https://github.com/SirHades696/Pypi_bot_telegram",
     ],
-    8: [
+    9: [
         "Mailer App📧",
         "Aplicación web desarrollada con Flask para enviar correos electrónicos con SendGrid y almacenarlos en una Base de Datos.",
         "https://github.com/SirHades696/flask_mailer",
     ],
-    9: [
+    10: [
         "Virtual Assistant🤖",
         "Asistente virtual desarrollado con Python, el cual realiza tareas sencillas de búsqueda en Wikipedia, reproduce música en YouTube, etc. ",
         "https://github.com/SirHades696/VirtualAssist",
     ],
-    10: [
+    11: [
         "Warzone Stats App🎮",
         "Aplicación desarrollada en Python la cual muestra las estadísticas de jugadores de Call of Duty: Warzone de diferentes plataformas.",
         "https://github.com/SirHades696/Warzone_python",
     ],
-    11: [
+    12: [
         "Basic Calculator📟",
         "Calculadora básica desarrolla con Python la cual permite sumar, restar multiplicar y dividir.",
         "https://github.com/SirHades696/Calculator",
     ],
-    12: [
+    13: [
         "Org. de Archivos📁",
         "Clasificador de archivos de acuerdo con su extensión agrupándolos en carpetas, por default clasifica los archivos ubicados en “Descargas”.",
         "https://github.com/SirHades696/ClasificadorArchivos",
     ],
-    13: [
+    14: [
         "DataScience Spotify🎶",
         "Script que analiza las canciones contenidas en una playlist para posteriormente realizar un gráfico basado en el tipo.",
         "https://github.com/SirHades696/Spotify_and_Python",
     ],
-    14: [
+    15: [
         "D.S. Pokemons🐞",
         "Clasificación de pokemons basados en sus características de ataque, poder, entre otros; esta clasificación fue realizada con Pandas.",
         "https://github.com/SirHades696/DataScience_Pokemons/",
     ],
-    15: [
+    16: [
         "D.S. Mundiales⚽",
         "DataScience aplicado a un histórico de mundiales extraídos desde un archivo de hoja de cálculo, obteniendo información  como: goles promedio, goleadores, etc.",
         "https://github.com/SirHades696/DataScience_",
